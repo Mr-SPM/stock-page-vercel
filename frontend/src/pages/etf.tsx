27d74f6e2fd86b45f735fc75c040079c1f660a43
weijie.com <vic.c2@outlook.com>
2026-04-16 05:30:57 +0000

    change detail page

--- frontend/src/pages/etf.tsx
+++ frontend/src/pages/etf.tsx
@@ -20,23 +20,28 @@ export default function ETFPage() {
 
     const navigate = useNavigate()
 
+
+    const goXueqiu = (symbol: string) => {
+        window.open(`https://xueqiu.com/S/${symbol}`)
+    }
+
     const dateCellRender = (value: Dayjs) => {
         const dateStr = value.format('YYYY-MM-DD')
         if (data[dateStr]) {
             return <ul className='my-date-list' onClick={(e) => e.stopPropagation()}>
-                {data[dateStr].map(item => <li onClick={() => navigate(`/charts/${dateStr.substring(0, 7)}/${item.symbol}`)} title={item.symbol} >{item.name}&nbsp;<span style={{ color: item.percent > 0 ? '#ff4d4f' : '#73d13d' }}>{item.percent}</span></li>)}
+                {data[dateStr].map(item => <li onClick={() => goXueqiu(item.symbol)} title={item.symbol} >{item.name}&nbsp;<span style={{ color: item.percent > 0 ? '#ff4d4f' : '#73d13d' }}>{item.percent}</span></li>)}
             </ul>
         }
     }
     return <ConfigProvider locale={zhCN}><div className='my-etf'>
-        <Button danger style={{marginBottom: 16}} onClick={() => navigate('/all/' + month)}>榜单</Button>
+        <Button danger style={{ marginBottom: 16 }} onClick={() => navigate('/all/' + month)}>榜单</Button>
         <Calendar dateCellRender={dateCellRender} style={{ borderRadius: 8 }} onSelect={(value) => {
-        setMonth(value.format('YYYY-MM'))
-        setDate(value);
-        setTimeout(() => {
-            setOpen(true);
-        },200)
-    }} />
+            setMonth(value.format('YYYY-MM'))
+            setDate(value);
+            setTimeout(() => {
+                setOpen(true);
+            }, 200)
+        }} />
         <EtfRecordModal
             open={open}
             onClose={() => setOpen(false)}
